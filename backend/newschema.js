d08014ebcaa77979e80624a7d30f3fee5445bdb5
vaibhav <vaibhavkashyap872@gmail.com>
2025-09-09 18:01:11 +0530

    Delete some Files

--- backend/newschema.js
+++
@@ -1,8 +0,0 @@
-const {z} =require("zod")
-
-
-const nameschema=z.string()
-
-let username=z.string().min(1,"Minimum 1 is required").max(10,"Please Enter less than 10")
-
-console.log(username.safeParse("456sdasdsdadsdsadsad"))
